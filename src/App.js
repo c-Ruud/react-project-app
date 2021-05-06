d33ef3bc63dbb2126234f6e4718e971be04b742c
c-Ruud <caroline.ruud@outlook.com>
2021-05-06 23:33:10 +0200

    React kurs från youtube

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import {Route, Switch} from 'react-router-dom'
+
+import NewMeetupPage from "./pages/NewMeetup";
+import FavoritesPages from "./pages/Favorites";
+import AllMeetupsPages from "./pages/AllMeetups";
+import Layout from "./components/layout/Layout";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <Layout>
+            <Switch>
+                <Route path='/' exact>
+                    <AllMeetupsPages/>
+                </Route>
+                <Route path='/new-meetup'>
+                    <NewMeetupPage/>
+                </Route>
+                <Route path='/favorites'>
+                    <FavoritesPages/>
+                </Route>
+            </Switch>
+        </Layout>
+    );
 }
 
 export default App;
